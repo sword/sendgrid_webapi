8b8ad1875540c1cc72c42e78c23a18ca022be5e4
Rob Sworder <robsworder@timeout.com>
2013-03-06 11:06:25 +0000

    disable vcr recording

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.default_cassette_options = { :record => :new_episodes }
+  #c.default_cassette_options = { :record => :new_episodes }
 end
 
 RSpec.configure do |c|
